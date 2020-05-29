310a35994cc526e81e7a10778f682e12a89360b3
agungdharmawangsa <60925593+agungdharmawangsa@users.noreply.github.com>
2020-05-29 19:44:53 +0800

    Add files via upload

---
+++ Final Project dharma 1708561045/simakUnud/index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+      crossorigin="anonymous"/>
+      <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet"> 
+    <link
+      href="https://fonts.googleapis.com/css?family=Poppins:300,400,700&display=swap"
+      rel="stylesheet"/>
+    <link rel="stylesheet" href="css/indexstyle.css" />
+    <title>Simak Udayana</title>
+  </head>
+  <body><div class="left-panel">
+    <section class="logo">
+      <img class="logounud" src="picture/unud.png" alt="unud.png" height="150px" />
+    </section>
+    <section class="sub-panel">
+      <h3>POPULAR ARTICLE</h3>
+    </section>
+    <section class="sub-panel-1">
+      <a href="#" style="text-decoration: none;">
+        <h4>TOP SEARCH</h4>
+      </a>
+    </section>
+    <section class="sub-panel-1">
+      <a href="#" style="text-decoration: none;">
+        <h4>DEPARTMENT NEWS</h4>
+      </a>
+    </section>
+    <section class="sub-panel-1">
+      <a href="#" style="text-decoration: none;">
+        <h4>UNUD INNOVATION</h4>
+      </a>
+    </section>
+    <section class="sub-panel-1">
+      <a href="#" style="text-decoration: none;">
+        <h4>UNUD DAILY</h4>
+      </a>
+    </section>
+  </div>
+  <div class="right-panel">
+
+    <section class="navbar">
+      <ul>
+        <li>
+          <a href="index.php" style="text-decoration: none;">
+            <h3>HOME</h3>
+          </a>
+        </li>
+        <li>
+          <a style="color: red;" href="login.php">
+            <h3>LOGIN</h3>
+          </a>
+        </li>
+        <li>
+          <a style="color: red;" href="register.php">
+            <h3>REGISTER</h3>
+          </a>
+        </li>
+      </ul>
+    </section>
+
+    <section class="cover">
+      <img src="picture/kampusdharma.png">
+    </section>
+    <section class="deskripsi">
+      <h1 style="text-align: center;">WELCOME</h1>
+      <h2 style="color: rgb(0, 0, 255);">Berita terkini</h2>
+      <img style="width: 300px; float: left; margin-right: 20px; margin-bottom: 30px;" src="./picture/nuclear.png" alt="#">
+      <p>peneliti unveritas udayana berhasil mengembangkan reaktor nuclear fussi pertama di indonesia, reaktor ini diharapkan
+      mampu menjadi pembangkit energi utama di indonesia di masa yang akan datang, walau masih dalam tahap pengembangan dan pengujian optimisme selalu hadir dari...</p>
+      <a href="#">Baca Selengkapnya</a>
+      <h2 style="color: rgb(0, 0, 255); clear: both;">Galeri</h2>
+      <img style="width: 300px; height: 200px; float: left; margin-right: 20px; margin-bottom: 30px;"
+        src="./picture/rsptn.png" alt="#">
+      <img style="width: 300px; height: 200px; float: left; margin-right: 20px; margin-bottom: 30px;"
+        src="./picture/rektordkk.png" alt="#">
+      <img style="width: 300px; height: 200px; float: left; margin-right: 20px; margin-bottom: 30px;"
+        src="./picture/ukm.png" alt="#">
+      <img style="width: 300px; height: 200px; float: left; margin-right: 20px; margin-bottom: 30px;"
+        src="./picture/lulus.png" alt="#">
+    </section>
+  </div>
+  <div class="footer">
+    <h2>UNIVERSITAS UDAYANA</h2>
+      <h4>Jl. Raya Kampus UNUD, Bukit Jimbaran, Kuta Selatan, Badung-Bali-803611</h4>
+      <h4>Phone Number: +62 (361) 701954, 704845</h4>
+      <h4>Fax: +62 (361) 701907</h4>
+      <h4>Email: info@unud.ac.id</h4>
+  </div>
+              
+  </body>
+
+  <script
+    src="https://code.jquery.com/jquery-3.4.1.js"
+    integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+    crossorigin="anonymous"
+  ></script>
+  <script
+    src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+    crossorigin="anonymous"
+  ></script>
+  <script
+    src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+    crossorigin="anonymous"
+  ></script>
+  <script
+    src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+    crossorigin="anonymous"
+  ></script>
+</html>
